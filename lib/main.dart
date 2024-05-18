7bbf10955e9d1afaca9e53dba9a17e2732e0b719
Evokoo <evanjohncooper@gmail.com>
2024-05-19 03:12:00 +1000

    EntryCard UI element mocked up

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:journal/widgets/entry_card.dart';
 import 'package:journal/database/entry_db.dart';
 
-void main() {
+final entryDB = EntryDB();
+
+void main() async {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  // Constructor
   const MainApp({super.key});
 
+  // Override Build method - this is the layout of the widget
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: TextButton(
-          onPressed: click,
-          child: Text("Hello"),
-        )),
-      ),
-    );
+    final List<EntryCard> entries = [];
+
+    for (int i = 0; i < 20; i++) {
+      entries.add(EntryCard(
+          title: "This is title $i",
+          body:
+              "Curabitur tristique purus lobortis, eleifend erat vitae, mollis ipsum. Mauris efficitur, magna at pharetra volutpat, ipsum nisl eleifend ex, at porttitor felis odio eu mi. Nam sed magna rhoncus, dictum ipsum nec, posuere purus. Vivamus sodales finibus consectetur. Nam imperdiet faucibus est vitae auctor"));
+    }
+
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Journal"),
+              centerTitle: true,
+              backgroundColor: Colors.blue[600],
+            ),
+            body: Container(
+                margin: const EdgeInsets.all(10),
+                child: ListView(children: entries))));
   }
-}
-
-void click() async {
-  print("Click");
-
-  var entryDB = EntryDB();
-
-  await entryDB.clearTable();
-  var entries = await entryDB.fetchAll();
-
-  print(entries);
 }
---
+++ lib/widgets/entry_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class EntryCard extends StatelessWidget {
+  final String title;
+  final String body;
+
+  const EntryCard({super.key, required this.title, required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: const EdgeInsets.all(5),
+        child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Column(
+                children: [_titleBar(title), _bodyContent(body), _date()])));
+  }
+
+  Widget _date() {
+    DateTime dt = DateTime.now();
+    String mm = dt.month.toString().padLeft(2, "0");
+    String dd = dt.day.toString();
+    String yyyy = dt.year.toString();
+    String min = dt.minute.toString().padLeft(2, "0");
+    String hour = dt.hour.toString();
+
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Row(
+          children: [
+            Text(
+              "$dd - $mm - $yyyy @ $hour:$min",
+              style: const TextStyle(fontSize: 12),
+            )
+          ],
+        ));
+  }
+
+  Widget _icons() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      Icon(Icons.edit, color: Colors.grey[400]),
+      Icon(Icons.highlight_remove, color: Colors.grey[400])
+    ]);
+  }
+
+  Widget _titleBar(String title) {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 3,
+                child: Text(
+                  title,
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w600, fontSize: 16),
+                )),
+            Expanded(flex: 1, child: _icons())
+          ],
+        ));
+  }
+
+  Widget _bodyContent(String body) {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Wrap(children: [
+          Text(
+            body,
+          )
+        ]));
+  }
+}
